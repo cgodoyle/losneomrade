41c731c6c979275519c0f418703f62efceeeb979
Cristian Godoy <cgodoy.le@gmail.com>
2023-10-24 10:27:13 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,88 @@
+# Quick Clay Release Area (Kvikkkleire løsneområde) -- Losneomrade package
+
+**A python package for calculating the maximum retrogression of landslides in quick-clay slopes**
+
+## Introduction
+
+![terrain criteria for landslide retrogression](terrain_criteria.png)
+
+This package is intended to be used for calculating the maximum retrogression of landslides in quick-clay. The package is based on the method described in [NVE Kvikkleireveileder 1/2019](https://publikasjoner.nve.no/veileder/2019/veileder2019_01.pdf), ie, assumes that the landslide retrogression is controlled by the slope of the terrain, and a slope of 1:15 is conservative enough to be used as a limit.
+
+Two modules are develop to calculate the maximum retrogression of landslides in quick-clay:
+* terrain_criteria: pixel-based verification of the terrain criteria (slope > 1:15) over the digital elevation model.
+* retrogression: step-wise calculation of the terrain criteria starting from the initial/source area.
+
+The digital elevation model (DEM) can be automatically fetched from Høydedata.no, or can be given as an input. Addinional option for clipping the results against the MSML (mulighet for sammenhengende marin leire: NGUs "mulig marin leire" without low probability/uncontinuous/thin layers) is also available. Both the slope criterium (default 1:15) and minimum height (default 5m) can be modified according to the user's needs.
+
+Both method should give similar results, but the terrain_criteria method is faster and less memory demanding. The retrogression method is computes a  consistent/continuous release area, but is slower. 
+
+Additionally, the package contains a module with some utility functions for rasterizing, polygonizing the results, query Høydedata.no and NVEs MSML.
+
+
+
+
+
+![terrain criteria for landslide retrogression](retrogression_animation.gif)
+
+
+
+## Installation
+Install the package using pip:
+```bash
+pip install git+https://github.com/cgodoyle/losneomrade.git
+```
+
+## Usage
+
+```python
+import numpy as np
+import geopandas as gpd
+from losneomrade import terrain_criteria, retrogression
+
+# define computation bounds
+xmin, xmax, ymin, ymax = 268463.9, 270007.6, 6651396.2, 6652564.4
+# define sourcepoints
+points  = np.array([[ 268883.54006222, 6651785.96167226],
+                    [ 268907.86379685, 6651662.57470644],
+                    [ 268984.48727134, 6651618.69204597],
+                    [ 269090.36143902, 6651686.56433795],
+                    [ 269207.33980788, 6651668.23740238],
+                    [ 269330.55864216, 6651689.9979547 ],
+                    [ 269423.18151256, 6651736.06328901],
+                    [ 269497.68960659, 6651807.85350652],
+                    [ 265169.57254198, 6648213.09660508]])
+
+tc = terrain_criteria.run_terrain_criteria(bounds=(xmin, xmax, ymin, ymax),
+                                           source=points,
+                                           source_depth=0.5,
+                                           clip_to_msml=True,
+                                           h_min=5
+                                          )
+
+
+# retrogression
+sourcepoints = gpd.GeodataFrame(
+    geometry=gpd.points_from_xy(points[:,0], points[:,1]),
+    crs=25833,
+    )
+akt=    retrogression.run_retrogression(
+            bounds = (xmin,xmax,ymin,ymax),
+            rel_shape=sourcepoints,
+            point_depth=0.5,
+            clip_to_msml=True,
+            min_slope=1/15,
+            min_length=75,
+            min_height=5,
+            return_animation=False,
+        )
+
+```
+
+## Feedback
+If you find a bug, or have any feedback, ideas or comments, please email me at crgo@nve.no.
+
+
+## References
+**NVE (2020), Veileder nr. 1/2019**. Sikkerhet mot kvikkleireskred : vurdering av områdestabilitet ved
+arealplanlegging og utbygging i områder med kvikkleire og andre jordarter med sprøbruddegenskaper.
+https://publikasjoner.nve.no/veileder/2019/veileder2019_01.pdf
---
+++ src/losneomrade/utils.py
@@ -0,0 +1,503 @@
+import os
+import tempfile
+import warnings
+from urllib.request import urlopen
+
+import rasterio
+import geopandas as gpd
+import numpy as np
+import plotly.graph_objects as go
+from rasterio import MemoryFile
+from rasterio.features import rasterize, shapes
+from scipy.spatial import distance_matrix
+import pandas as pd
+
+
+warnings.simplefilter(action='ignore', category=UserWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+HOYDEDATA_LAYER = "NHM_DTM_25833"
+
+
+def get_hoydedata(bounds: tuple, layer: str = HOYDEDATA_LAYER, res: int = 5, nodata: int = -9999) -> dict:
+    """
+    Function for downloading DEM from www.høydedata.no.
+
+    Args:
+        bounds (tuple): Bounding box of the DEM to be downloaden in the form of xmin, xmax, ymin, ymax
+        layer (str, optional): Which Høydedata API layer. "dtm1_32_wcs", "dtm1_33_wcs", "dtm1_32_wcs" or "dtm1_33_wcs".
+                               Defaults to "dtm1_33_wcs".
+        res (int, optional): Resolution of the output DEM in meters, if it is different from the layer used it
+                            will be resampled. Defaults to 5.
+        nodata (int, optional): Value for nodata pixels. Defaults to -9999.
+
+    Returns:
+        (tuple): (dem_array, transform): array with DEM values and the transform used to convert array's rows
+                                         and cols to geographic coordinates.
+    """
+
+    # Check input layer
+    if layer not in ["dtm1_32_wcs", "dtm1_33_wcs", "dtm10_32_wcs", "dtm10_33_wcs", "NHM_DTM_25833", "NHM_DTM_25832"]:
+        print("Use a valid API layer (check help).")
+        return dict()
+
+    # Set up request to høydedata
+    xmin, xmax, ymin, ymax = bounds
+    width = int((xmax - xmin) / res)
+    height = int((ymax - ymin) / res)
+
+    request_url = (
+        f"https://hoydedata.no/arcgis/rest/services/{layer}/ImageServer/exportImage?"
+        f"bbox={xmin},{ymin},{xmax},{ymax}&size={width},{height}&bboxSR=&size=&imageSR=&time=&format=tiff&"
+        f"pixelType=F32&noData={nodata}&noDataInterpretation=esriNoDataMatchAny"
+        f"&interpolation=+RSP_BilinearInterpolation&compression=&compressionQuality=&"
+        f"bandIds=&mosaicRule=&renderingRule=&f=image"
+    )
+
+    # Open the request output with rasterio and save elevation array and transform
+    try:
+        tif_bytes = urlopen(request_url).read()
+
+        windows_dems = []
+        windows_transforms = []
+
+        with MemoryFile(tif_bytes) as memfile:
+            with memfile.open() as dataset:
+                dataset_profile = dataset.profile
+                windows = [window for ij, window in dataset.block_windows()]
+                full_array = dataset.read(1)
+                for window in windows:
+                    windows_dems.append(dataset.read(1, window=window))
+
+                    windows_transforms.append(
+                        rasterio.windows.transform(window, dataset.transform)
+                    )
+
+    except Exception as e:
+        print("Error (Probably area requested is too big/small or høydedata is down)")
+        raise
+
+    return {
+        "windows_dem_arrays": windows_dems,
+        "windows_transforms": windows_transforms,
+        "windows": windows,
+        "profile": dataset_profile,
+        "full_array": full_array
+    }
+
+
+def dem_coordinates(dem_array: np.ndarray, dem_transform: rasterio.transform.Affine) -> np.ndarray:
+    """
+    get coordinates of the given dem window
+    Args:
+        dem_array: window's elevation array
+        dem_transform: window's transform
+
+    Returns:
+        coords: numpy array with the coordinates (x,y,z) of the dem
+    """
+    height, width = dem_array.shape
+    cols, rows = np.meshgrid(np.arange(width), np.arange(height))
+    xs, ys = rasterio.transform.xy(dem_transform, rows, cols)
+    x_dem = np.array(xs)
+    y_dem = np.array(ys)
+    coords = np.c_[x_dem.flatten(), y_dem.flatten(), dem_array.flatten()]
+    return coords
+
+
+def compute_slope(coords: np.ndarray, points: np.ndarray, h_min: float = 5, nodata: int = -9999) -> np.ndarray:
+    """
+    Compute the slopes of the given dem with respect to the (source) points
+    Args:
+        coords: dem window coordinates
+        points: source point coordinates
+        h_min: minimum height difference where slopes are calculated
+        nodata: value given to pixels with no data
+
+    Returns:
+        max_slope: array with slopes (same shape as input dem)
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        xy_1 = coords[:, :2]
+        xy_2 = points[:, :2]
+        z1 = coords[:, -1]
+        z2 = points[:, -1]
+        distance_mtx = distance_matrix(xy_1, xy_2)
+        height_mtx = z1[:, np.newaxis] - z2
+        hl_ratio = height_mtx / distance_mtx
+        hl_ratio[height_mtx < h_min] = nodata
+        max_slope = np.max(hl_ratio, axis=1)
+
+        return max_slope
+
+
+def set_z_from_raster(points_xy: np.ndarray, window_data: dict) -> np.ndarray:
+    """
+    Set elevation value to the given x,y points
+    Args:
+        points_xy: numpy array with the x,y coordinates to the points
+        window_data: DEM-results from calling get_hoydedata function
+
+    Returns: numpy array with x,y,z coordinates
+
+    """
+    height = window_data["profile"]["height"]
+    width = window_data["profile"]["width"]
+    dem_array = window_data["full_array"]
+    transform = window_data["profile"]["transform"]
+
+    bounds = rasterio.transform.array_bounds(height, width, transform)
+    points_filt = points_xy[
+        (points_xy[:, 0] > bounds[0])
+        & (points_xy[:, 0] < bounds[2])
+        & (points_xy[:, 1] > bounds[1])
+        & (points_xy[:, 1] < bounds[3])
+    ]
+
+    index = np.array([rasterio.transform.rowcol(transform, xx[0], xx[1]) for xx in points_filt])
+    z = np.array([dem_array[xx[0], xx[1]] for xx in index])
+    filter_nan = z == window_data["profile"]["nodata"]
+
+    return np.c_[points_filt[:, :2], z][~filter_nan]
+
+
+def profile(line, dtm_layer=HOYDEDATA_LAYER, nodata=-9999, fra_crs=4326, to_crs=25833):
+    """
+    Compute a terrain profile from a given line
+    Args:
+        line: array with xy coordinates
+        dtm_layer: høydedata api layer
+        nodata: value to be used as nodata
+        fra_crs: input crs
+        to_crs: output crs
+
+    Returns: numpy array with X, Y, Z, M values
+
+    """
+    gdf = gpd.GeoDataFrame(
+        index=[0], crs=f"epsg:{int(fra_crs)}", geometry=[line]
+    ).to_crs(epsg=to_crs)
+    linea2 = gdf.iloc[0].geometry
+    n_points = int(max(linea2.length / 5, 5))
+    new_points = [
+        linea2.interpolate(i / float(n_points - 1), normalized=True)
+        for i in range(n_points)
+    ]
+    points_coords = np.array(
+        [[pp.coords.xy[0][0], pp.coords.xy[1][0]] for pp in new_points]
+    )
+
+    xmin, ymin = np.min(points_coords, axis=0) - 10
+    xmax, ymax = np.max(points_coords, axis=0) + 10
+    width = int((xmax - xmin) / 5)
+    height = int((ymax - ymin) / 5)
+
+    request_url = (
+        f"https://hoydedata.no/arcgis/rest/services/{dtm_layer}/ImageServer/exportImage?"
+        f"bbox={xmin},{ymin},{xmax},{ymax}&size={width},"
+        f"{height}&bboxSR=&size=&imageSR=&time=&format=tiff&pixelType=F32&"
+        f"noData={nodata}&noDataInterpretation=esriNoDataMatchAny"
+        f"&interpolation=+RSP_BilinearInterpolation&compression=&"
+        f"compressionQuality=&bandIds=&mosaicRule=&renderingRule=&f=image"
+    )
+    tif_bytes = urlopen(request_url).read()
+
+    z_dem = []
+
+    try:
+        with MemoryFile(tif_bytes) as memfile:
+            with memfile.open() as dataset:
+                dem_array = dataset.read(1)
+                for pp in points_coords:
+                    ind = dataset.index(pp[0], pp[1])
+                    z_dem.append(dem_array[ind[0], ind[1]])
+
+    except rasterio.errors.RasterioIOError:
+        print("feil")
+
+    cum_dist = np.cumsum(
+        np.sqrt(
+            np.sum(
+                (np.r_[[[0, 0]], np.diff(points_coords, axis=0)[:, :2]]) ** 2, axis=1
+            )
+        )
+    )
+
+    points = np.c_[points_coords, z_dem, cum_dist]
+    return points
+
+
+def generate_plotly_profile(prof, max_depth=None, kp_depth=0, limit=15):
+    """
+    Generate a plotly figure with the profile
+    Args:
+        prof: numpy array with X, Y, Z, M values
+        max_depth: maximum depth to be used for the base of the profile
+
+    Returns:
+        fig: plotly figure
+
+    """
+    base_depth = (prof[:, -2].min() - (prof[:, -2].max() - prof[:, -2].min())) if max_depth is None else max_depth
+    base = np.ones_like(prof[:, -2]) * base_depth
+
+    m_line, z_line = generate_terraincriteria_line(prof, depth=kp_depth, limit=limit)
+
+    traces = [go.Scatter(x=prof[:, -1], y=base,
+                         line_color="rgba(161, 99, 18, 0.7)", fillcolor="rgba(161, 99, 18, 0.7)",
+                         mode="lines", ),
+              go.Scatter(x=prof[:, -1], y=prof[:, -2],
+                         fill="tonexty", line_color="black", fillcolor="rgba(161, 99, 18, 0.7)",
+                         mode="lines", ),
+              go.Scatter(x=m_line, y=z_line, line=dict(dash='dash', color="rgba(0, 0, 0, 0.7)", width=0.7, ),
+                         mode="lines", text=f"1:{int(limit)}-line"),
+              ]
+
+    fig = go.Figure().add_traces(traces)
+    coords_title = f"({prof[0, 0]:.1f}, {prof[0, 1]:.1f}) - ({prof[-1, 0]:.1f}, {prof[-1, 1]:.1f})"
+    fig.update_layout(title=f"Profile @ {coords_title}",
+                      font=dict(size=10),
+                      showlegend=False,
+                      margin=dict(l=0, r=0, t=20, b=20, autoexpand=True),
+                      dragmode="pan", width=600, height=210, )
+    fig.update_annotations(font_size=10)
+    return fig
+
+
+def generate_terraincriteria_line(prof, limit=15, depth=0):
+    """
+    Generate a line with the terrain criteria (by default 1:15)
+    Args:
+        prof: numpy array with X, Y, Z, M values
+        limit: Limit in vertical/horizontal ratio (1:limit) to be used for the terrain criteria
+        depth: depth to be used for the line
+
+    Returns:
+        m: numpy array with the M values
+        z: numpy array with the Z values
+    """
+    z = prof[:, -2]
+    m = prof[:, -1]
+    i_min, z_min = np.argmin(z), z.min()
+    m_0 = m[i_min]
+    m_line = np.abs(m - m_0)
+    z_line = z_line = z_min + m_line / limit - depth
+    z_line[z_line > prof[:, -2]] = np.nan
+
+    return m, z_line
+
+
+def polygonize_results(result_array: np.ndarray, dem_profile: rasterio.profiles.Profile,
+                       field="value", threshold_value=1):
+    """
+    Polygonize an array whose values are greather tha the given threshold and save the results as a 
+    geopandas dataframe with the given field name.
+
+    Args:
+        result_array: array with the results
+        dem_profile: profile of the dem
+        field: field name to be used for the polygonized results
+        threshold_value: value to be used as threshold for the polygonization
+
+    Returns:
+        gpd_polygonized_raster: geopandas dataframe with the polygonized results
+    """
+
+    raster_transform = dem_profile['transform']
+    raster_crs = dem_profile['crs']
+
+    results_binary = (result_array >= threshold_value).astype(int)
+
+    results = ({"properties": {"id": i, field: int(v)}, "geometry": s}
+               for i, (s, v) in enumerate(shapes(results_binary, mask=None, transform=raster_transform)))
+    geoms = list(results)
+    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms)
+    gpd_polygonized_raster = gpd_polygonized_raster[gpd_polygonized_raster[field] >= threshold_value]
+    gpd_polygonized_raster = gpd_polygonized_raster.set_crs(raster_crs)
+    return gpd_polygonized_raster
+
+
+def rasterize_shape(release_shp, dem_profile: rasterio.profiles.Profile) -> np.ndarray:
+    """
+    Rasterize the release area
+    Args:
+        release_shp: release area as a geopandas dataframe
+        dem_profile: profile of the dem
+
+    Returns:
+        rasterized: rasterized release area as a numpy array
+    """
+    dem_height = dem_profile['height']
+    dem_width = dem_profile['width']
+    dem_transform = dem_profile['transform']
+
+    geom = [shapes_ii for shapes_ii in release_shp.geometry]
+
+    rasterized = rasterize(geom,
+                           out_shape=(dem_height, dem_width),
+                           fill=0,
+                           out=None,
+                           transform=dem_transform,
+                           all_touched=True,
+                           default_value=1,
+                           dtype=None)
+
+    return rasterized
+
+
+def get_msml_mask(bounds: tuple, dem_profile: rasterio.profiles.Profile = None):
+    """
+    Get the MSML mask as an array for the given bounds
+    Args:
+        bounds: tuple with the bounds (xmin, ymin, xmax, ymax)
+        dem_profile: profile of the dem. If None, returns the mask as a geopandas dataframe
+
+    Returns:
+        mask_array: array with the MSML mask or a geopandas dataframe if dem_profile is None
+    """
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        xmin, ymin, xmax, ymax = bounds
+
+        # Get MSML mask
+        url_nve_msml = "https://gis3.nve.no/map/rest/services/Mapservices/MarinGrense/MapServer/7/query?" \
+                       "geometry=xmin%3A+{}%2C+ymin%3A+{}%2C+xmax%3A+{}%2C+ymax%3A+{}&" \
+                       "geometryType=esriGeometryEnvelope&f=geojson"
+        query_msml_mask = url_nve_msml.format(xmin, ymin, xmax, ymax)
+        response = urlopen(query_msml_mask)
+        json_mask = response.read()
+
+        with open(os.path.join(tempdir, "msml_mask.json"), "wb") as file_json_mask:
+            file_json_mask.write(json_mask)
+        mask_msml = gpd.read_file(os.path.join(tempdir, "msml_mask.json"), driver='GeoJSON').to_crs(epsg=25833)
+
+        # Get Areal under MG mask
+        url_nve_aumg = "https://gis3.nve.no/map/rest/services/Mapservices/MarinGrense/MapServer/8/query?" \
+                       "geometry=xmin%3A+{}%2C+ymin%3A+{}%2C+xmax%3A+{}%2C+ymax%3A+{}&" \
+                       "geometryType=esriGeometryEnvelope&f=geojson"
+        query_aumg_mask = url_nve_aumg.format(xmin, ymin, xmax, ymax)
+        response_2 = urlopen(query_aumg_mask)
+        json_mask_2 = response_2.read()
+        with open(os.path.join(tempdir, "marin_mask.json"), "wb") as file_json_mask:
+            file_json_mask.write(json_mask_2)
+        mask_aumg = gpd.read_file(os.path.join(tempdir, "marin_mask.json"), driver='GeoJSON').to_crs(epsg=25833)
+
+        # concatenate masks
+        mask_gpd = gpd.GeoDataFrame(pd.concat([mask_msml, mask_aumg], ignore_index=True)).set_crs(epsg=25833)
+        if dem_profile is None:
+            return mask_gpd
+
+        if len(mask_gpd) == 0:
+            mask_array = None
+        else:
+            mask_array = rasterize_shape(mask_gpd, dem_profile)
+
+        return mask_array
+
+
+def generate_windows(custom_raster: str):
+    """
+    Generate windows from a raster in the same way get_hoydedata does.
+    Args:
+        custom_raster: path to the raster
+
+    Returns:
+        dictionary with the windows, transforms, dem arrays and the profile of the raster
+    """
+    blockxsize, blockysize = 640, 640
+
+    with rasterio.open(custom_raster) as src:
+        dataset_profile = src.profile
+        full_array = src.read(1)
+        dataset_transform = src.transform
+
+        windows_dems = []
+        windows_transforms = []
+
+        block_shapes = np.array(src.block_shapes).squeeze()
+
+        if np.any(block_shapes == 1):
+            with tempfile.TemporaryDirectory() as temp_folder:
+                dataset_profile.update(blockxsize=blockxsize, blockysize=blockysize, tiled=True)
+                new_raster = os.path.join(temp_folder, "reblocked")
+                with rasterio.open(new_raster, "w+", **dataset_profile) as out:
+                    out.write(full_array, indexes=1)
+                return generate_windows(new_raster)
+
+        windows = [window for _, window in src.block_windows()]
+
+        for window in windows:
+            data = src.read(1, window=window)
+            windows_dems.append(data)
+            windows_transforms.append(
+                rasterio.windows.transform(window, dataset_transform)
+            )
+
+    return {
+        "windows_dem_arrays": windows_dems,
+        "windows_transforms": windows_transforms,
+        "windows": windows,
+        "profile": dataset_profile,
+        "full_array": full_array
+    }
+
+
+def convert_lines_to_gpd(lines: set) -> gpd.GeoDataFrame:
+    """
+    Function to convert a line into a geopandas dataframe.
+    It is used to save the profiles that are being plotted.
+
+    Args:
+        lines (set): a set object with the lines taken from the map.
+
+    Returns:
+        gpd.GeoDataFrame: dataframe in wgs coordinate system
+    """
+    from shapely.geometry import LineString
+    line_shapes = []
+    line_id = []
+    for index, line in enumerate(lines):
+        line_np = np.array(line)
+        line_shapes.append(LineString(line_np))
+        line_id.append(index)
+
+    gdf = gpd.GeoDataFrame(index=line_id, crs="epsg:4326", geometry=line_shapes)
+
+    return gdf
+
+
+def generate_fake_slope(base_length, base_elevation, terrace_length, terrace_elevation, slope_ratio, xmin=2e5, ymax=6e6):
+
+    resolution = 1
+    # Calculate the length of the flat base and the sloped region
+    slope_length = (terrace_elevation - base_elevation) // slope_ratio
+    width = base_length + slope_length + terrace_length
+
+    # Create the flat base
+    base_array = np.full((int(width), int(base_length)), base_elevation)
+
+    # Create the slope
+    slope_array = np.linspace(base_elevation, terrace_elevation, int(slope_length))
+    slope_array = np.tile(slope_array, (int(width), 1))
+
+    # Create the terrace
+    terrace_array = np.full((int(width), int(terrace_length)), terrace_elevation)
+
+    # Concatenate the three sections to create the full terrain
+    dem = np.hstack((base_array, slope_array, terrace_array))
+
+    transform = rasterio.transform.from_origin(xmin, ymax, resolution, resolution)
+
+    profile = {
+        'driver': 'GTiff',
+        'height': dem.shape[0],
+        'width': dem.shape[1],
+        'count': 1,
+        'dtype': 'float32',
+        'crs': 'EPSG:25833',
+        'transform': transform,
+        'nodata': -9999,
+    }
+
+    return dem, profile
